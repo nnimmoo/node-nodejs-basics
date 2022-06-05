0e79f48d69ee5fc9bbe6830356fbca8415aaa2eb
nimo <76771466+nnimmoo@users.noreply.github.com>
2022-06-05 23:09:54 +0400

    Update transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,12 @@
+import { Transform} from 'stream';
 export const transform = async () => {
-    // Write your code here 
+    const trans = new Transform({
+       transform(chunk,enc,cb){
+           const temp = chunk.toString().trim();
+           trans.push(temp.split('').reverse().join(''));
+           cb();
+       }
+   });
+   process.stdin.pipe(trans).pipe(process.stdout);
 };
+transform();

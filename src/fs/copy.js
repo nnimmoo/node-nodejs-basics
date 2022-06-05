7393df37d2a717f02bda0865f65d9514aec99569
nimo <76771466+nnimmoo@users.noreply.github.com>
2022-06-05 23:03:35 +0400

    Update copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,17 @@
+import{mkdirSync,readdirSync,lstatSync,copyFileSync,existsSync} from 'fs';
+import { join } from 'path';
+
 export const copy = async () => {
-    // Write your code here 
-};
+    if (!existsSync('./files')||existsSync('./files_copy')) {
+        console.log('FS operation failed');
+    } else {
+    mkdirSync('./files_copy');
+    readdirSync('./files').forEach(element => {
+        if (lstatSync(join('./files', element)).isFile()) {
+            copyFileSync(join('./files', element), join("./files_copy", element));
+        } else {
+            copyFolderSync(join('./files', element), join('./files_copy', element));
+        }
+    });
+}};
+copy();

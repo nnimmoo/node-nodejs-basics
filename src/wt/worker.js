29cc506db1441bdaaa6f9e4513089c43619fdb18
nimo <76771466+nnimmoo@users.noreply.github.com>
2022-06-05 23:17:11 +0400

    Update worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,13 @@
+import { parentPort, workerData } from 'worker_threads';
+
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
+    parentPort.postMessage(
+        nthFibonacci(workerData.num)
+      );
     // This function sends result of nthFibonacci computations to main thread
 };
+
+sendResult();

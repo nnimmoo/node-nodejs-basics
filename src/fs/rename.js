363796d213e10c8f25811c5811afc8dd98e055e2
nimo <76771466+nnimmoo@users.noreply.github.com>
2022-06-05 23:05:33 +0400

    Update rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,13 @@
+import { renameSync, existsSync } from 'fs';
 export const rename = async () => {
-    // Write your code here 
+    if (!existsSync('./files/wrongFilename.txt') ||existsSync('./files/properFilename.md')){
+        console.log('FS operation failed');
+    } else {
+        renameSync('./files/wrongFilename.txt', './files/properFilename.md', function (err) {
+        if (err) throw err;
+        console.log('Renamed!');
+      });
+    }
 };
+
+rename();

12b15285b115fbadc7778e1fecb5179729946e1e
nimo <76771466+nnimmoo@users.noreply.github.com>
2022-06-05 23:09:11 +0400

    Update decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,11 @@
+import {createGunzip} from "zlib";
+import { createReadStream, createWriteStream} from "fs";
+
 export const decompress = async () => {
-    // Write your code here 
+    const gzip = createGunzip();
+    
+    const input = createReadStream("./files/archive.gz");
+    const output = createWriteStream("./files/fileToCompress.txt");
+
+    input.pipe(gzip).pipe(output);
 };

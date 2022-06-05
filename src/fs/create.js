4951b5f42fe8c138f31f796d64b9c53de4d3a850
nimo <76771466+nnimmoo@users.noreply.github.com>
2022-06-05 23:04:06 +0400

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,15 @@
+import { appendFile, existsSync } from 'fs';
+
+
 export const create = async () => {
-    // Write your code here 
+    if (existsSync('./files/fresh.txt')) {
+        console.log('FS operation failed');
+    } else {
+    appendFile('./files/fresh.txt', ' I am fresh and young', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+    }
 };
+
+create();

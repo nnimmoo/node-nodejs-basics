601f2fae277336caca88b94d12ee66dd913f2c32
nimo <76771466+nnimmoo@users.noreply.github.com>
2022-06-05 23:04:55 +0400

    Update list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,20 @@
+import { readdir, existsSync} from 'fs';
 export const list = async () => {
-    // Write your code here 
+    if (!existsSync('./files')) {
+        console.log('FS operation failed');
+    } else {
+    readdir('./files', function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        var arr = [];
+        files.forEach(function (file) {
+            arr.push(file);
+        }
+        ); 
+        console.log(arr.toString()); 
+    });
+}
 };
+
+list();

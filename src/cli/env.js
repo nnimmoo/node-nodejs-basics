7998875ec5d66560d069280cae88fc483b065150
nimo <76771466+nnimmoo@users.noreply.github.com>
2022-06-05 23:24:56 +0400

    Update env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
+
 export const parseEnv = () => {
-    // Write your code here 
+    for (let obj of Object.entries(process.env)) {
+        if (obj[0].startsWith('RSS_')) {
+          console.log(eobjnv[0] +'=' + obj[1]);
+          
+        }
+      }
 };
+
+parseEnv();

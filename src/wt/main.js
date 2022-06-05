da3d97b27ecf16b6cc13152c0af1ac982a9708f6
nimo <76771466+nnimmoo@users.noreply.github.com>
2022-06-05 23:16:54 +0400

    Update main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,21 @@
+import { Worker } from 'worker_threads';
 export const performCalculations = async () => {
-    // Write your code here
+    
+    var num =10;
+
+    for (let index = 0; index < 4; index++) {
+    
+    const worker = new Worker("./worker.js", {workerData: {num: num}});
+
+    worker.once("message", result => {
+        console.log({status: "resolved", data:result});
+    });
+    worker.on("error", error => {
+        console.log({status: "error", data:null });
+    });
+   
+    num++;
+}
 };
+
+performCalculations();

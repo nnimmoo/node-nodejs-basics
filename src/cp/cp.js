3c64911c2aaa67c7b88317b166255ab524a8ce38
nimo <76771466+nnimmoo@users.noreply.github.com>
2022-06-05 23:00:18 +0400

    Update cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,10 @@
+import {fork} from 'child_process';
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+
+  const child = fork("./files/script.js", args);
+
+  child.send(args);
+
 };
+//gets array of inputs from terminal
+spawnChildProcess(process.argv.slice(2));

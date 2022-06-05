c365038a1b97c1fed5119b13c24ea50231b3d256
nimo <76771466+nnimmoo@users.noreply.github.com>
2022-06-05 23:05:17 +0400

    Update read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,14 @@
+import { readFile, existsSync } from 'fs';
+
 export const read = async () => {
-    // Write your code here 
+    if (!existsSync('./files/fileToRead.txt')) {
+        console.log('FS operation failed');
+    } else {
+        readFile('./files/fileToRead.txt', function (err, data) {
+        if (err) throw err;
+        console.log(data.toString());
+      });
+    }
 };
+
+read();

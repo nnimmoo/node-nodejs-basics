0796d99e6113f62ac42692a1dfecb78a06a467de
nimo <76771466+nnimmoo@users.noreply.github.com>
2022-06-05 22:59:09 +0400

    Update args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,6 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const myArgs = process.argv.slice(2);
+    for (let i = 0; i < myArgs.length; i=i+2) {
+    console.log (myArgs[i].slice(2)+' is '+ myArgs[i+1]);
+    }
 };
